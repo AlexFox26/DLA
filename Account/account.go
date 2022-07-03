49b6cee2dc3a8876261e1fc9a6b5b8ce466796e5
AlexFox26 <105880947+AlexFox26@users.noreply.github.com>
2022-07-03 21:11:20 +0300

    Add Account + Transaction

---
+++ Account/account.go
@@ -0,0 +1,51 @@
+package account
+
+import (
+	"blockchain/key"
+	"blockchain/transaction"
+
+	"crypto/sha1"
+	"strconv"
+	"time"
+)
+var IDAutoIncrementation uint64 = 0//в качестве уникального ID
+
+type Account struct{
+	ID uint32
+	keys keypair.Key
+	votesNumber uint64
+	dateLastVote time.Time
+}
+
+//создание ключей
+func (objAcc *Account)GetAccount(){
+	objAcc.ID = uint32(IDAutoIncrementation)
+	IDAutoIncrementation++
+	objAcc.keys.GenerateKeys()
+	objAcc.votesNumber=0
+}
+
+//обновление ключа пользователя
+func (objAcc *Account)AddKeyPair(){
+	objAcc.keys.GenerateKeys()
+}
+
+//узнать количество голосов за пользователя
+func (objAcc *Account)GetBalance( ) uint64{
+	return objAcc.votesNumber
+}
+
+//функция для получения голоса
+func (objAcc *Account)GetVote(){
+	objAcc.votesNumber++
+}
+
+//функция для голосования за другого пользователя
+func (objAcc *Account)Vote(reseiver *Account){
+	var message string //формируем стоку, где просто говорим о том, что я хочу проголосовать за такого-то пользователя (необходяма просто для подтверждения транзакции)
+	message = "Person " + strconv.FormatUint(uint64(objAcc.ID), 10) + " vote for person "+ strconv.FormatUint(uint64(reseiver.ID), 10)
+	sha1.New()
+	resHach := sha1.Sum([]byte(message)) //хешируем наше сообщение
+	r,s:=objAcc.keys.SingData(resHach)	//подписываем
+	transaction.Vote(objAcc, reseiver,r,s, resHach)	//отправляем на проверку и подписание транзакции
+}
